363923854b04211f435728b5b45e2b7b8e28cd7d
Jonathan Dantas <jonathandantasads@outlook.com>
2021-02-10 14:58:15 -0300

    Primeiro desafio concluido

---
+++ testeRomano/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testeRomano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int R = 0;
+
+            Console.Write("Digite o número romano em MAIUSCULO: ");
+            string n = Console.ReadLine();
+
+            int Indices = n.Length;
+
+
+
+            for (int z = 0; z < Indices; z++)
+            {
+                if (n.Length == z + 1)
+                {
+                    for (int i = 0; i < n.Length; i++)
+                    {
+                        char k = n[i];
+
+                        if (k == 'I')
+                        {
+                            R = R + 1;
+                        }
+
+                        else if (k == 'V')
+                        {
+                            R = R + 5;
+                        }
+
+                        else if (k == 'X')
+                        {
+                            R = R + 10;
+                        }
+                    }
+                }
+            }
+
+            int i1 = n.LastIndexOf("I");
+            int i2 = n.LastIndexOf("V");
+            int i3 = n.LastIndexOf("X");
+
+            if (i1 != -1)
+            {
+                if (i1 < i2 || i1 < i3)
+                {
+                    R = R - 2;
+                }
+            }
+
+            Console.WriteLine(R);
+        }
+    }
+}
